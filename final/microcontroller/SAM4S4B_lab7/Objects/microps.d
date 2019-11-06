167c22999eec4ded7508eb9c2deafc216abb8219
artrujillo <40245917+artrujillo@users.noreply.github.com>
2019-11-05 23:13:54 -0800

    set up Keil environment and set up the rot encoder.
    Wrote code that programs the rot encoder to toggle between two LEDs depending on the direction the user spins it.

---
+++ final/microcontroller/SAM4S4B_lab7/Objects/microps.d
@@ -0,0 +1,16 @@
+.\objects\microps.o: microps.c
+.\objects\microps.o: SAM4S4B.h
+.\objects\microps.o: SAM4S4B_sys.h
+.\objects\microps.o: SAM4S4B_pmc.h
+.\objects\microps.o: f:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\microps.o: SAM4S4B_pio.h
+.\objects\microps.o: SAM4S4B_tc.h
+.\objects\microps.o: SAM4S4B_uart.h
+.\objects\microps.o: SAM4S4B_pwm.h
+.\objects\microps.o: SAM4S4B_rtc.h
+.\objects\microps.o: SAM4S4B_wdt.h
+.\objects\microps.o: SAM4S4B_spi.h
+.\objects\microps.o: SAM4S4B_adc.h
+.\objects\microps.o: f:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\objects\microps.o: f:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\microps.o: f:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
--- final/microcontroller/SAM4S4B_lab7/microps.c
+++ final/microcontroller/SAM4S4B_lab7/microps.c
@@ -7,49 +7,58 @@ Fall 2019
  http://eeshop.unl.edu/pdf/KEYES%20Rotary%20encoder%20module%20KY-040.pdf
 */
 
-#include "SAM4S4B_pio.h"
+#include "SAM4S4B.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-#DEFINE pinCW = PIOA_PA15; // Connected to CLK on KY­040
-#DEFINE pinCCW = PIOA_PA16; // Connected to DT on KY­040
+#define pinCCW PIO_PA15 // Connected to DT on KY­040
+#define pinCW PIO_PA16 // Connected to CLK on KY­040
+#define ledCW PIO_PA17
+#define ledCCW PIO_PA18
 
-int encoderPosCount = 0;
-int pinCWLast;
-int aVal;
-boolean bCW;
-
-void setup_rot_encoder() {
-pinMode(pinCW,INPUT);
-pinMode(pinCCW,INPUT);
-/* Read Pin A
-Whatever state it's in will reflect the last position
-*/
-pinCWLast = digitalRead(pinCW);
+int setup_rot_encoder() {
+	pioPinMode(pinCW, PIO_INPUT);
+	pioPinMode(pinCCW, PIO_INPUT);
+	/* Read Pin A
+	Whatever state it's in will reflect the last position
+	*/
+	return pioDigitalRead(pinCW);
 }
 
-int main() {
-    setup_rot_encoder();
-    for(1){
-        rot = digitalRead(pinCW);
-        if (rot != pinCWLast){ // Means the knob is rotating
-            // if the knob is rotating, we need to determine direction
+int main(void) {
+	uint16_t pinCWLast;
+	uint16_t rotCW;
+	uint16_t rot;
+	samInit();
+	pioInit();
+	
+  pinCWLast = setup_rot_encoder();
+	
+	pioPinMode(ledCW, PIO_OUTPUT);
+	pioPinMode(ledCCW, PIO_OUTPUT);
+	pioDigitalWrite(ledCW, 0);
+	pioDigitalWrite(ledCCW, 0);
+  while(1){
+      rot = pioDigitalRead(pinCW);
+      if (rot != pinCWLast){ // Means the knob is rotating
+          // if the knob is rotating, we need to determine direction
             // We do that by reading pin B.
-            if (digitalRead(pinCCW) != rot) { // Means pin A Changed first ­ We're Rotating Clockwise
-            encoderPosCount++;
-            rotCW = true;
-            } else { // Otherwise B changed first and we're moving CCW
-            rotCW = false;
-            encoderPosCount­­--;
+          if (pioDigitalRead(pinCCW) != rot) { // Means pin A Changed first ­ We're Rotating Clockwise
+						rotCW = 1;
+          } else { // Otherwise B changed first and we're moving CCW
+            rotCW = 0;
+          }
+          if (rotCW){
+							pioDigitalWrite(ledCW, 1);
+							pioDigitalWrite(ledCCW, 0);
+          }else{
+							pioDigitalWrite(ledCW, 0);
+							pioDigitalWrite(ledCCW, 1);
             }
-            Serial.print ("Rotated: ");
-            if (rotCW){
-            Serial.println ("clockwise");
-            }else{
-            Serial.println("counterclockwise");
-            }
-            Serial.print("Encoder Position: ");
-            Serial.println(encoderPosCount);
+            
+            
         }
-        pinCWLast = rot
+      pinCWLast = rot;
     }
-return 0
 }
