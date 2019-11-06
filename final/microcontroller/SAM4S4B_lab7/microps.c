bc66c545fc5209f33df443a072f0161c95fc3743
artrujillo <40245917+artrujillo@users.noreply.github.com>
2019-11-06 15:56:52 -0800

    wroked on the c code for rot encoder
    pins are not working as proper inputs

--- final/microcontroller/SAM4S4B_lab7/microps.c
+++ final/microcontroller/SAM4S4B_lab7/microps.c
@@ -12,8 +12,8 @@ Fall 2019
 #include <stdlib.h>
 #include <stdio.h>
 
-#define pinCCW PIO_PA15 // Connected to DT on KY­040
-#define pinCW PIO_PA16 // Connected to CLK on KY­040
+#define pinCCW PIO_PA9 // Connected to DT on KY­040
+#define pinCW PIO_PA10 // Connected to CLK on KY­040
 #define ledCW PIO_PA17
 #define ledCCW PIO_PA18
 
@@ -27,38 +27,30 @@ int setup_rot_encoder() {
 }
 
 int main(void) {
-	uint16_t pinCWLast;
-	uint16_t rotCW;
-	uint16_t rot;
+	uint16_t CW;
+	uint16_t CCW;
 	samInit();
 	pioInit();
 	
-  pinCWLast = setup_rot_encoder();
+  CW = setup_rot_encoder();
 	
 	pioPinMode(ledCW, PIO_OUTPUT);
 	pioPinMode(ledCCW, PIO_OUTPUT);
 	pioDigitalWrite(ledCW, 0);
 	pioDigitalWrite(ledCCW, 0);
   while(1){
-      rot = pioDigitalRead(pinCW);
-      if (rot != pinCWLast){ // Means the knob is rotating
+      CW = pioDigitalRead(pinCW);
+		  CCW = pioDigitalRead(pinCCW);
+      if (CW != CCW){ // Means the knob is rotating
           // if the knob is rotating, we need to determine direction
             // We do that by reading pin B.
-          if (pioDigitalRead(pinCCW) != rot) { // Means pin A Changed first ­ We're Rotating Clockwise
-						rotCW = 1;
+          if (CW == 0) { // Means pin A Changed first ­ We're Rotating Clockwise
+						pioDigitalWrite(ledCW, 1);
+						pioDigitalWrite(ledCCW, 0);
           } else { // Otherwise B changed first and we're moving CCW
-            rotCW = 0;
+            pioDigitalWrite(ledCW, 0);
+						pioDigitalWrite(ledCCW, 1);
           }
-          if (rotCW){
-							pioDigitalWrite(ledCW, 1);
-							pioDigitalWrite(ledCCW, 0);
-          }else{
-							pioDigitalWrite(ledCW, 0);
-							pioDigitalWrite(ledCCW, 1);
-            }
-            
-            
         }
-      pinCWLast = rot;
     }
 }
